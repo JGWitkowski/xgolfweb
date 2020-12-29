545c8f6bb8068cf513ed38100e8c6ff35921c8c4
Justin Witkowski <justinw@voatz.com>
2020-12-29 12:35:48 -0500

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+    @ViewChild('toggleOne') toggleOne: ElementRef | undefined;
+  @ViewChild('toggleOneTarget') toggleOneTarget: ElementRef | undefined;
+      @ViewChild('toggleTwo') toggleTwo: ElementRef | undefined;
+  @ViewChild('toggleTwoTarget') toggleTwoTarget: ElementRef | undefined;
+
+  showNav = [false,false];
+  clickNav(number: number) {
+    console.log(number);
+  this.showNav[number] = !this.showNav[number]; 
+}
+
+  constructor(private renderer: Renderer2) { 
+
+    this.renderer.listen('window', 'click',(e:Event)=>{
+        if(e.target !== this.toggleOne!.nativeElement && e.target!==this.toggleOneTarget!.nativeElement){
+            this.showNav[0] = false;
+        }
+              if(e.target !== this.toggleTwo!.nativeElement && e.target!==this.toggleTwoTarget!.nativeElement){
+            this.showNav[1] = false;
+        }
+    });
+  }
+
+  // toggleMenu() {
+  //   this.isMenuOpen = !this.isMenuOpen;
+  // }
+
+  ngOnInit(): void {
+  }
+
+}
