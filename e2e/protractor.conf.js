4a4c2f4f82fe9b3f852ce5cce15a5a9629048cef
Justin Witkowski <justinw@voatz.com>
2020-12-30 22:33:01 -0500

    prod ready

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: '0.0.0.0:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
